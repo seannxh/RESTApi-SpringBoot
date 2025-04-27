a446e39db1581048b2f83908654776a361a0eebb
seannxh <nohsean6@gmail.com>
2025-04-26 22:05:57 -0500

    PathVariable Param

--- src/main/java/com/restapi/springboot/restapi/controller/RestfulWebServiceApplication.java
+++ src/main/java/com/restapi/springboot/restapi/controller/RestfulWebServiceApplication.java
@@ -2,6 +2,7 @@ package com.restapi.springboot.restapi.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,13 @@ public class RestfulWebServiceApplication {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello World");
+    }
+
+    //{x} <= anything inside the {} parameter in the path is a Path parameter
+    //use %s or %f or %d in String.format to represent the variable that will be replaced by a param name
+    //String format makes the code format look cleaner
+    @GetMapping(path = "/hello-world-bean/path-var/{name}")
+    public HelloWorldBean helloWorldPathVar(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
