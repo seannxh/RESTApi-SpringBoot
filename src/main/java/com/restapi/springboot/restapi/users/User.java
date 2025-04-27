68766f72054bf8889d8175c77a8d2703628de597
seannxh <nohsean6@gmail.com>
2025-04-26 22:15:56 -0500

    Created User Bean

---
+++ src/main/java/com/restapi/springboot/restapi/users/User.java
@@ -0,0 +1,44 @@
+package com.restapi.springboot.restapi.users;
+
+import java.time.LocalDate;
+
+public class User {
+
+    private final Integer id;
+
+    private String name;
+
+    private LocalDate birthDate;
+    public Integer getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public User(Integer id, String name, LocalDate birthDate) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+}
