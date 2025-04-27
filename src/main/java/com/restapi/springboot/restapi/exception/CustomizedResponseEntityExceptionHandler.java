81b150fcea3daa62ce9c70aade22799a6de22cad
seannxh <nohsean6@gmail.com>
2025-04-26 23:44:07 -0500

    Created Customer Exception for User and for All

---
+++ src/main/java/com/restapi/springboot/restapi/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.restapi.springboot.restapi.exception;
+
+import com.restapi.springboot.restapi.users.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+
+}
---
+++ src/main/java/com/restapi/springboot/restapi/exception/ErrorDetails.java
@@ -0,0 +1,37 @@
+package com.restapi.springboot.restapi.exception;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class ErrorDetails {
+
+    //Structure for my error class
+    //timestamp
+    //message
+    //details
+
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+
+    public ErrorDetails( LocalDateTime timestamp, String message, String details) {
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+
+}
