ef450c9f016fb8ed7394a8bb2e2581ba2871b937
seannxh <nohsean6@gmail.com>
2025-04-27 19:26:43 -0500

    Static & Dynamic Serializtion logic w/ Filter and MappingJacksonValue, SimpleBeanProviderFilter

--- src/main/java/com/restapi/springboot/restapi/filtering/FilteringController.java
+++ src/main/java/com/restapi/springboot/restapi/filtering/FilteringController.java
@@ -1,5 +1,9 @@
 package com.restapi.springboot.restapi.filtering;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +13,13 @@ import java.util.List;
 @RestController
 public class FilteringController {
     @GetMapping("/filtering")
-    public SomeBean filtering(){
-        return new SomeBean("value1", "value2", "value3");
+    public MappingJacksonValue filtering(){
+        SomeBean someBean = new SomeBean("value1", "value2", "value3");
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field3");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
+        mappingJacksonValue.setFilters(filters);
+        return mappingJacksonValue;
     }
 
     @GetMapping("/filtering-list")
--- src/main/java/com/restapi/springboot/restapi/filtering/SomeBean.java
+++ src/main/java/com/restapi/springboot/restapi/filtering/SomeBean.java
@@ -1,12 +1,14 @@
 package com.restapi.springboot.restapi.filtering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
-@JsonIgnoreProperties("field2")
+//@JsonIgnoreProperties("field2")
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
-    @JsonIgnore
+    //@JsonIgnore
     private String field1;
     private String field2;
     private String field3;
@@ -21,6 +23,18 @@ public class SomeBean {
 
     public String getField3() {
         return field3;
+    }
+
+    public void setField2(String field2) {
+        this.field2 = field2;
+    }
+
+    public void setField1(String field1) {
+        this.field1 = field1;
+    }
+
+    public void setField3(String field3) {
+        this.field3 = field3;
     }
 
     @Override
