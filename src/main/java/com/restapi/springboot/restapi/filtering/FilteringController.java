533267ffbdce37aa8e59cdc5fb56a6f6b7de5255
seannxh <nohsean6@gmail.com>
2025-04-27 19:40:56 -0500

    Spring Actuator

--- src/main/java/com/restapi/springboot/restapi/filtering/FilteringController.java
+++ src/main/java/com/restapi/springboot/restapi/filtering/FilteringController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -23,8 +24,14 @@ public class FilteringController {
     }
 
     @GetMapping("/filtering-list")
-    public List<SomeBean> filteringList(){
-        return Arrays.asList(new SomeBean("value1", "value2", "value3"),
+    public MappingJacksonValue filteringList(){
+        List<SomeBean> list = Arrays.asList(
+                new SomeBean("value1", "value2", "value3"),
                 new SomeBean("value4", "value5", "value6"));
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field2", "field3");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(list);
+        mappingJacksonValue.setFilters(filters);
+        return mappingJacksonValue;
     }
 }
